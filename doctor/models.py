c92fb0ca1dfdc88aa39d9434dd8865687a981ffc
kacfal <kacfal181195@gmail.com>
2019-03-05 22:07:35 +0100

    Modify model doctor

--- doctor/models.py
+++ doctor/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Doctor(models.Model):
     name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
+    office_number = models.IntegerField()
+
+    def __unicode__(self):
+        return f'{self.id}. {self.name}'
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'

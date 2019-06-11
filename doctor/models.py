7296df02973bf23eae1d076f7fc8904a332f6732
kacfal <kacfal181195@gmail.com>
2019-06-11 23:09:11 +0200

    Hot FIX

--- doctor/models.py
+++ doctor/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Doctor(models.Model):
     name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
-    office_number = models.PositiveSmallIntegerField(unique=True)
+    office_number = models.PositiveIntegerField(validators=[MinValueValidator(1)], unique=True)
 
     def __unicode__(self):
         return f'{self.id}. {self.name} {self.last_name}'
